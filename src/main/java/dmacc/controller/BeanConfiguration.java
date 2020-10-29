6e91bf1425e7456ce0244f69359091827ff58214
Walkingdeadj <jshi@dmacc.edu>
2020-10-29 02:49:58 -0500

    Lab2

---
+++ src/main/java/dmacc/controller/BeanConfiguration.java
@@ -0,0 +1,33 @@
+package dmacc.controller;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import dmacc.beans.Address;
+import dmacc.beans.Contact;
+
+@Configuration
+public class BeanConfiguration {
+	/**
+	@Bean
+	public Contact contact() {
+		Contact bean = new Contact( );
+		return bean;
+	}
+	*/
+	
+	@Bean
+	public Contact contact() {
+		Contact bean = new Contact("Eric Carle");
+		//bean.setName("Dr. Seuss");
+		//bean.setPhone("555-555-5555");
+		//bean.setRelationship("friend");
+		return bean;
+	}
+	
+	@Bean
+	public Address address() {
+		Address bean = new Address("123 Main Street", "DesMoines", "IA");
+		return bean;
+	}
+}
